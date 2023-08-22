42a39b8bca1df246bd6d1afefc25379ae79afade
vdukhovnik <vdukhovnik1@gmail.com>
2023-08-22 20:33:48 +0200

    test task 05.

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -27,7 +27,7 @@ function Pages() {
                 <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                <Route path="/" element={<Error404/>}/>
+                <Route path="*" element={<Error404/>}/>
             </Routes>
         </div>
     )
